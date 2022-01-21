9ca0f7f3cabac17c73024202e7f906369f7c9e3d
Linus Sundström <linus.sundstrom@aalto.fi>
2022-01-21 14:58:35 +0200

    Tehtävä 1.3

--- osa1/kurssiteidot/src/App.js
+++ osa1/kurssiteidot/src/App.js
@@ -39,18 +39,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} count1={exercises1} part2={part2} count2={exercises2} part3={part3} count3={exercises3} />
-      <Total num1={exercises1} num2={exercises2} num3={exercises3} />
+      <Content part1={part1.name} count1={part1.exercises} part2={part2.name} count2={part2.exercises} part3={part3.name} count3={part3.exercises} />
+      <Total num1={part1.exercises} num2={part2.exercises} num3={part3.exercises} />
     </div>
   )
 }
